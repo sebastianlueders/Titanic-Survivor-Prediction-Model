92c0b8c36befa9fc39f58b1c4fea3011fde9616c
Seb Lueders <38769233+sebastianlueders@users.noreply.github.com>
2024-09-03 15:32:43 -0500

    init finished & initial method headers added.

--- titanic/HW1.py
+++ titanic/HW1.py
@@ -1,3 +1,26 @@
-import sys
-print(sys.executable)
-print(sys.version)
+import numpy as np
+
+class AdalineModel:
+    """
+    Adaline Classifier for Titanic Survivor Prediction
+
+    Parameters:
+
+
+    Atributes:
+
+
+    """
+
+    def __init__(self, learning_rate=0.1, epochs=100):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+
+
+    #def fit(self, x, y):
+
+    #def net_input(self, x):
+
+    #def activate(self, y):
+
+    #def predict(self, x):
